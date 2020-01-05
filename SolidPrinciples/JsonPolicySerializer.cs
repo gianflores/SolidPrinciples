0159099c6e8d6ff019a5de6d0b88813f61a7b059
gianflores <gian_flores@hotmail.com>
2020-01-05 07:39:42 -0500

    Refactor using single responsibility principle

---
+++ SolidPrinciples/JsonPolicySerializer.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidPrinciples
+{
+
+    public class JsonPolicySerializer
+    {
+        public Policy GetPolicyFromJsonString(string jsonString)
+        {
+            return JsonConvert.DeserializeObject<Policy>(jsonString,
+                new StringEnumConverter());
+        }
+    }
+}
