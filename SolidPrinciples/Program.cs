57eed62598b6b8035127be8675b45452e96ced9f
gianflores <gian_flores@hotmail.com>
2020-01-05 07:20:42 -0500

    Add code with no SOLID principles applied

--- SolidPrinciples/Program.cs
+++ SolidPrinciples/Program.cs
@@ -6,6 +6,17 @@ namespace SolidPrinciples
     {
         static void Main(string[] args)
         {
+            var engine = new RatingEngine();
+            engine.Rate();
+
+            if (engine.Rating > 0)
+            {
+                Console.WriteLine($"Rating: {engine.Rating}");
+            }
+            else
+            {
+                Console.WriteLine("No rating produced.");
+            }
         }
     }
 }
